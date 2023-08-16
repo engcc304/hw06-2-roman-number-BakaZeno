ffd3c4317d7c24ef3d6147b5e9b51d2025ca1cba
BakaZeno <agarz3no@gmail.com>
2023-08-16 19:22:32 +0700

    test

--- Homework/HW06/hw06-2-roman-number.cpp
+++ Homework/HW06/hw06-2-roman-number.cpp
@@ -23,3 +23,48 @@
         929 = CMXXIX
 
 */
+#include <stdio.h>
+#include <string.h>
+
+const char* convertToRoman(int num) {
+    static char roman_num[100]; // เก็บผลลัพธ์ที่ได้ในรูปของสตริง
+    int val[] = {
+        1000, 900, 500, 400,
+        100, 90, 50, 40,
+        10, 9, 5, 4,
+        1
+    };
+    const char* syb[] = {
+        "M", "CM", "D", "CD",
+        "C", "XC", "L", "XL",
+        "X", "IX", "V", "IV",
+        "I"
+    };
+    roman_num[0] = '\0'; // ล้างสตริงที่เก็บผลลัพธ์
+    int i = 0;
+    while (num > 0) {
+        for (int j = 0; j < num / val[i]; j++) {
+            strcat(roman_num, syb[i]);
+            num -= val[i];
+        }
+        i++;
+    }
+    return roman_num;
+}
+
+int main() {
+    int number;
+    printf("กรุณากรอกตัวเลข: ");
+    scanf("%d", &number);
+
+    const char* roman_result = convertToRoman(number);
+
+    printf("%d = %s\n", number, roman_result);
+
+    return 0;
+}
+
+
+
+
+
